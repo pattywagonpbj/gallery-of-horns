d9781ef5499e3feb50c7ecbafbd74a2fbc98e219
pattywagonpbj <patrick.jarrett11@gmail.com>
2021-03-20 18:41:15 -0700

    add header, main, and footer tag

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Horned Beasts</h1>
+        </header>
+        <main>          
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -14,10 +17,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
-      </header>
+          </main>
+      <footer>Patrick Jarrett</footer>
     </div>
   );
 }
